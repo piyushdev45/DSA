9d5e1e4e404d818f96b7d7af7f6b14e49174cee4
piyush <piyushcodes45@gmail.com>
2026-02-11 16:04:26 +0530

    added new file

---
+++ variable and datatype.py
@@ -0,0 +1,19 @@
+#name ="piyush"
+#age =19
+#print("my name is:",name)
+#print(type(name))
+#print(type(age))
+# adding
+"""a=2
+b=5
+sum = a+b
+print(sum)"""
+#arithmetic operators
+"""a= 2000
+b= 1000
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)"""
+ 
