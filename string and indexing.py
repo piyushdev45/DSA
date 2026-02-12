80467b24be8b56ff9c40d773155895788033da5a
piyush <piyushcodes45@gmail.com>
2026-02-13 01:26:43 +0530

    perfect

---
+++ string and indexing.py
@@ -0,0 +1,12 @@
+str1="new"
+str2="technology"
+print(str1+str2)
+# WE SPERATE THE LINE IN STRING BY USING \n
+str3="this is AI era\nand we accept it"
+print(str3)
+# BY USING (len(str)) WE FIND THE LENGTH OF str
+print(len(str3))
+# INDEXING 
+str4="ai and ml student "
+ch =str4[7]
+print(ch)
