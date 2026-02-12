d86ab1cc934eef5093186f792c393ecfc2646f71
piyush <piyushcodes45@gmail.com>
2026-02-13 02:13:13 +0530

    s.s.f

---
+++ slicing and string function.py
@@ -0,0 +1,15 @@
+#SLICING AND BACKWARD SLICING
+str1= "daily commit "
+print(str1[1:3])
+print(str1[-9:-4])
+# STRING FUNCTION 
+# FIRST IS ENDSWITH FUNCTION (if the word are same as in str gives true otherwise give false)
+str2="daily try to do more"
+print(str2.endswith("more"))
+#CAPITALIZE FUNCTION
+str3="the RUTHLESS RANDY ORTON"
+print(str3.capitalize())
+#REPLACE 
+print(str3.replace("o","p"))
+#COUNT FUNCTION 
+print(str3.count("the"))
