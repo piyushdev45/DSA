b47a0577c38fe020d08b6cff59fb252ba99dd276
piyush <piyushcodes45@gmail.com>
2026-02-14 00:02:01 +0530

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+name =["piyush",5.6,"student"]
+print(name[2])
+#NAME CHANGE
+name[0]="abhinav"
+print(name)
+# LIST SILCING
+print(name[0:2])
+#LIST METHOD
+name.append(98.7)#APPEND METHOD
+print(name)
+f=["banna","apple"]
+print(f.sort(reverse=True))
