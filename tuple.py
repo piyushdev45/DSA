5313074caa47e110eff12998f741ac986b481f51
piyush <piyushcodes45@gmail.com>
2026-02-14 23:49:04 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,16 @@
+# Simple tuple
+numbers = (1, 2, 3, 4, 5)
+print(numbers)
+data = ("Vishu", 21, 5.8, True)
+print(data)
+colors = ("red", "green", "blue")
+print(colors[0])   # first element
+print(colors[-1])  # last element
+cities = ("Delhi", "Mumbai", "Chennai")
+print(len(cities))
+games = ("cricket", "football", "hockey")
+
+if "cricket" in games:
+    print("Cricket is available")
+nested = ((1, 2), (3, 4), (5, 6))
+print(nested[1][0])  # Output: 3
