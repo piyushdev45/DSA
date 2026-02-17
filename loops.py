575f88ca99de75389afd49b9ea294143b17009fd
piyush <piyushcodes45@gmail.com>
2026-02-17 23:33:23 +0530

    added new file

--- loops.py
+++ loops.py
@@ -1,7 +1,8 @@
 list1 = [45,65,48,2,4,5,2]
-
+list2 = []
 for i in list1:
     if i%2==0:
-      print(i,end=",")
+      list2.append(i)
     else:
-       print(None,end=",")
+       list2.append("none")
+print(list2)
