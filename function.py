f5299cf3cde0dc69785c54f928334df1c930f1d7
piyush <piyushcodes45@gmail.com>
2026-02-19 23:04:26 +0530

    function

--- function.py
+++ function.py
@@ -2,5 +2,14 @@ def calc_sum(a,b):
     sum = a+b
     print(sum)
     return(sum)
-calc_sum(5,7)
+
+calc_sum(55,77)
+
+name =["piyush","abhinav","yash","jeevanshu"]
+def len(name):
+    print(len)
+number=['1','2','3','4','5']   
+print(number,end=" ")
+
+    
     
