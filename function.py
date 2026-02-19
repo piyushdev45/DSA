e843282b7f4a58fbe57027bb5c1ccab12ee60f33
piyush <piyushcodes45@gmail.com>
2026-02-19 23:11:31 +0530

    fact

--- function.py
+++ function.py
@@ -1,15 +1,20 @@
-def calc_sum(a,b):
-    sum = a+b
-    print(sum)
-    return(sum)
+#def calc_sum(a,b):
+#    sum = a+b
+#    print(sum)
+#    return(sum)
+#calc_sum(55,77)
 
-calc_sum(55,77)
-
-name =["piyush","abhinav","yash","jeevanshu"]
-def len(name):
-    print(len)
-number=['1','2','3','4','5']   
-print(number,end=" ")
+#name =["piyush","abhinav","yash","jeevanshu"]
+#def len(name):
+#    print(len)
+#number=['1','2','3','4','5']   
+#print(number,end=" ")
+def calc_fact(n):
+    fact=1
+    for i in range(1,n+1):
+        fact *=i
+        print(fact)
+calc_fact(4)
 
     
     
