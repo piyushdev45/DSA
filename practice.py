f2d19329f36b2aeb4ebbdb02dbf1e99dd043052d
piyush <piyushcodes45@gmail.com>
2026-02-21 21:35:49 +0530

    p

--- practice.py
+++ practice.py
@@ -15,3 +15,11 @@ def convertor(mass_val):
   gram_val=mass_val*1000
   print(f"{mass_val}kg is {gram_val}g")
 convertor(3.5)
+
+n= int(input("enter your number"))
+if n%5==0:
+  print("multiple of 5")
+elif n%11==0:
+  print("multiple of 11")
+else:
+  print("not multiple of 5 and 11")
