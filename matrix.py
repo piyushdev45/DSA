4d7e16f45f3781c0a583a6e231fcb0c365961e82
piyush <piyushcodes45@gmail.com>
2026-02-24 22:46:56 +0530

    matrix

--- matrix.py
+++ matrix.py
@@ -6,3 +6,12 @@ for j in range (len(b[0])):
     for k in range(len(b)):
       result_mul[i][j]+=a[i][k]*b[k][j]
 print("multiplication:\n",result_mul)
+
+a=[[4,5,6],[3,4,5]]
+b=[[2,3,4],[6,7,8]]
+result_mul =[[0 for _ in range (len(b[0]))]for _ in range (len(a))]
+for j in range (len(b[0])):
+  for i in range(len(a)):
+    for k in range(len(b)):
+      result_mul[i][j]+=a[i][k]*b[k][j]
+print("multiplication:\n",result_mul)
