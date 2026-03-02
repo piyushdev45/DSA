6412fc37a42cdd3884dfa03e7f0a0ddf588f189b
piyush <piyushcodes45@gmail.com>
2026-03-03 00:56:11 +0530

    array

--- array.py
+++ array.py
@@ -15,3 +15,16 @@ print(arr)
 arr = [1, 2, 3, 4, 5]
 arr.reverse()
 print(arr)
+arr = [1, 2, 3, 4, 5, 6]
+
+even = []
+odd = []
+
+for i in arr:
+    if i % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+
+print("Even:", even)
+print("Odd:", odd)
