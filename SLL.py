a06d3b5e6dc63e1877cd5f233d9215bc47cd103c
piyush <piyushcodes45@gmail.com>
2026-03-11 23:36:45 +0530

    SLL

--- SLL.py
+++ SLL.py
@@ -1,2 +1,8 @@
 list=[1,2,3,4,5]
 print(list)
+#NODE 
+class Node:
+    def __init__(self,data):
+        self.data=data(3)
+        self.next=None(3)
+        print("Node created with data:",self.data)
