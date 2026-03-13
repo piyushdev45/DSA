f83ad9e5e8dbd97498da935dd4b84c7d8db2ecc7
piyush <piyushcodes45@gmail.com>
2026-03-13 23:22:08 +0530

    random

--- practice.py
+++ practice.py
@@ -146,4 +146,12 @@ print("name:",n)
 print("age:",d)
 
 
+import random 
+def generate_random_number(start, end):
+    return random.randint(start, end)
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+random_number = generate_random_number(start, end)
+print(f"Generated random number between {start} and {end}: {random_number}")
+
 
