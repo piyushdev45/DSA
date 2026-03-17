b87e184427d82ad7b6e353b3116e40a8805c8973
piyush <piyushcodes45@gmail.com>
2026-03-17 23:20:22 +0530

    numpy

--- num.py
+++ num.py
@@ -57,3 +57,19 @@ a = np.array(vals, dtype=dtype)
 
 print(np.sort(a, order='name'))
 print(np.sort(a, order=['year', 'cgpa']))
+
+
+import numpy as np
+ 
+arr = np.array([[-1, 2, 0, 4],
+                [4, -0.5, 6, 0],
+                [2.6, 0, 7, 8],
+                [3, -7, 4, 2.0]])
+
+arr2 = arr[:2, ::2]
+print ("first 2 rows and alternate columns(0 and 2):\n", arr2)
+ 
+arr3 = arr[[1, 1, 0, 3], 
+                [3, 2, 1, 0]]
+print ("\nElements at indices (1, 3), "
+    "(1, 2), (0, 1), (3, 0):\n", arr3)
