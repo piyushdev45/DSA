c0e5d953ebd52f1d59ba16a5331faaa0c8623710
piyush <piyushcodes45@gmail.com>
2026-03-20 23:28:32 +0530

    pandas25

--- pandas25.py
+++ pandas25.py
@@ -1,3 +1,13 @@
 import pandas as pd
 df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
 print(df)
+
+import pandas as pd 
+import numpy as np
+
+s = pd.Series() 
+print("Pandas Series: ", s) 
+data = np.array(['g', 'e', 'e', 'k', 's']) 
+  
+s = pd.Series(data) 
+print("Pandas Series:\n", s)
