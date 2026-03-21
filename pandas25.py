cd210c7ab170acab43a0c41e3065fb540d886d74
piyush <piyushcodes45@gmail.com>
2026-03-21 19:20:11 +0530

    pandas

--- pandas25.py
+++ pandas25.py
@@ -1,13 +1,23 @@
-import pandas as pd
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-print(df)
+# import pandas as pd
+# df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+# print(df)
+
+# import pandas as pd 
+# import numpy as np
+
+# s = pd.Series() 
+# print("Pandas Series: ", s) 
+# data = np.array(['g', 'e', 'e', 'k', 's']) 
+  
+# s = pd.Series(data) 
+# print("Pandas Series:\n", s)
+
 
 import pandas as pd 
-import numpy as np
-
-s = pd.Series() 
-print("Pandas Series: ", s) 
-data = np.array(['g', 'e', 'e', 'k', 's']) 
+   
+df = pd.DataFrame() 
+print(df)
+lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
   
-s = pd.Series(data) 
-print("Pandas Series:\n", s)
+df = pd.DataFrame(lst) 
+print(df)
