84cccb1613b80cfc35192b55445edf8e7ce5e68d
piyush <piyushcodes45@gmail.com>
2026-03-22 23:28:56 +0530

    password

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+    
+    for i in range(length):
+        password += random.choice(characters)
+        
+    return password
+
+print(generate_password(10))
