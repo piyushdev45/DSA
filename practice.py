19b88f1b0c9b1ae2ead6a970142e6ed3e5dca2f5
piyush <piyushcodes45@gmail.com>
2026-03-22 23:28:05 +0530

    practice

--- practice.py
+++ practice.py
@@ -166,3 +166,18 @@
 # c1=client("john",17)
 # c2=client("doe",25)  
 
+
+
+def frequency(lst):
+    freq = {}
+    
+    for item in lst:
+        if item in freq:
+            freq[item] += 1
+        else:
+            freq[item] = 1
+            
+    return freq
+
+nums = [1, 2, 2, 3, 3, 3, 4]
+print(frequency(nums))
