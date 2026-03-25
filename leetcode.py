8c732a3f034d07a5f971401f36806a75d0469913
piyush <piyushcodes45@gmail.com>
2026-03-25 22:37:04 +0530

    leetcode

--- leetcode.py
+++ leetcode.py
@@ -1,36 +1,41 @@
-x = 121
+# x = 121
 
-if str(x) == str(x)[::-1]:
-    print(True)
-else:
-    print(False)
+# if str(x) == str(x)[::-1]:
+#     print(True)
+# else:
+#     print(False)
 
-nums = [1,1,2]
+# nums = [1,1,2]
 
-nums = list(set(nums))
-print(nums)
+# nums = list(set(nums))
+# print(nums)
 
-n = 5
+# n = 5
 
-a = 1
-b = 1
+# a = 1
+# b = 1
 
-for i in range(n):
-    a, b = b, a+b
+# for i in range(n):
+#     a, b = b, a+b
 
-print(a)
+# print(a)
 
-def fizzBuzz(n):
-    result = []
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return result
+# def fizzBuzz(n):
+#     result = []
+#     for i in range(1, n + 1):
+#         if i % 15 == 0:
+#             result.append("FizzBuzz")
+#         elif i % 3 == 0:
+#             result.append("Fizz")
+#         elif i % 5 == 0:
+#             result.append("Buzz")
+#         else:
+#             result.append(str(i))
+#     return result
 
-print(fizzBuzz(15))
+# print(fizzBuzz(15))
+
+def findMin(nums):
+    return min(nums)
+
+print(findMin([3, 1, 7, 2]))
