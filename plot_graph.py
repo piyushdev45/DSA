8f2e81e72596e684cb76fa4110c9ac2354875680
piyush <piyushcodes45@gmail.com>
2026-04-09 22:39:44 +0530

    grid

--- plot_graph.py
+++ plot_graph.py
@@ -58,11 +58,22 @@
 # plt.ylabel('Temperature (°C)')  
 # plt.show()
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([1, 2, 3, 4])   
+# y = x * 2                
+
+# plt.plot(x, y)
+# plt.show()
+
 import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array([1, 2, 3, 4])   
-y = x * 2                
-
-plt.plot(x, y)
-plt.show()
+import numpy as np  
+x = np.array([1, 2, 3, 4])
+y= np.array([2, 4, 6, 8])
+plt.plot(x, y, marker='o', linestyle='-', color='purple')   
+plt.title('Line Plot Example')
+plt.xlabel('X-axis')    
+plt.ylabel('Y-axis')
+plt.grid()  
+plt.show() 
