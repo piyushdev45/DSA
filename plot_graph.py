7a59dd2bed9985bbdf68da364e1a4eded818f9fa
piyush <piyushcodes45@gmail.com>
2026-04-10 19:52:40 +0530

    scatterr

--- plot_graph.py
+++ plot_graph.py
@@ -108,31 +108,42 @@
 # plt.ylabel('Values (USD)')  
 # plt.show()
 
-import numpy as np 
-import matplotlib.pyplot as plt 
+# import numpy as np 
+# import matplotlib.pyplot as plt 
 
-barWidth = 0.25
-fig = plt.subplots(figsize =(12, 8)) 
+# barWidth = 0.25
+# fig = plt.subplots(figsize =(12, 8)) 
 
-IT = [12, 30, 1, 8, 22] 
-ECE = [28, 6, 16, 5, 10] 
-CSE = [29, 3, 24, 25, 17] 
+# IT = [12, 30, 1, 8, 22] 
+# ECE = [28, 6, 16, 5, 10] 
+# CSE = [29, 3, 24, 25, 17] 
 
-br1 = np.arange(len(IT)) 
-br2 = [x + barWidth for x in br1] 
-br3 = [x + barWidth for x in br2] 
+# br1 = np.arange(len(IT)) 
+# br2 = [x + barWidth for x in br1] 
+# br3 = [x + barWidth for x in br2] 
 
-plt.bar(br1, IT, color ='r', width = barWidth, 
-        edgecolor ='grey', label ='IT') 
-plt.bar(br2, ECE, color ='g', width = barWidth, 
-        edgecolor ='grey', label ='ECE') 
-plt.bar(br3, CSE, color ='b', width = barWidth, 
-        edgecolor ='grey', label ='CSE') 
+# plt.bar(br1, IT, color ='r', width = barWidth, 
+#         edgecolor ='grey', label ='IT') 
+# plt.bar(br2, ECE, color ='g', width = barWidth, 
+#         edgecolor ='grey', label ='ECE') 
+# plt.bar(br3, CSE, color ='b', width = barWidth, 
+#         edgecolor ='grey', label ='CSE') 
 
-plt.xlabel('Branch', fontweight ='bold', fontsize = 15) 
-plt.ylabel('Students passed', fontweight ='bold', fontsize = 15) 
-plt.xticks([r + barWidth for r in range(len(IT))], 
-        ['2015', '2016', '2017', '2018', '2019'])
+# plt.xlabel('Branch', fontweight ='bold', fontsize = 15) 
+# plt.ylabel('Students passed', fontweight ='bold', fontsize = 15) 
+# plt.xticks([r + barWidth for r in range(len(IT))], 
+#         ['2015', '2016', '2017', '2018', '2019'])
 
-plt.legend()
+# plt.legend()
+# plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+x= np.array([1, 2, 3, 4, 5])
+y= np.array([2, 4, 6, 8, 10])
+plt.scatter(x, y, color='blue', marker='x')
+plt.xlabel('X-axis')    
+plt.ylabel('Y-axis')
+plt.title('Simple Scatter Plot')    
+plt.grid()
 plt.show()
