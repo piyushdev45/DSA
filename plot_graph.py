327976ef2b8a98dc9fbf5b1263364921120b4f5d
piyush <piyushcodes45@gmail.com>
2026-04-12 23:51:05 +0530

    dark

--- plot_graph.py
+++ plot_graph.py
@@ -276,10 +276,18 @@
 
 # plt.show()
 
+# import numpy as np 
+# import matplotlib.pyplot as plt 
+# from matplotlib import style 
+# data = np.random.randn(50) 
+# plt.style.use('Solarize_Light2') 
+# plt.plot(data) 
+# plt.show()
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 from matplotlib import style 
 data = np.random.randn(50) 
-plt.style.use('Solarize_Light2') 
+plt.style.use('dark_background') 
 plt.plot(data) 
 plt.show()
