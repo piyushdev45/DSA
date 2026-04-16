c832539f1c606b5560afdcb4d116d8641582d5c0
piyush <piyushcodes45@gmail.com>
2026-04-16 23:42:44 +0530

    3d graph

--- 3d graph .py
+++ 3d graph .py
@@ -2,13 +2,25 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+
+# z = np.linspace(0, 1, 100)
+# x = z * np.sin(25 * z)
+# y = z * np.cos(25 * z)
+
+# ax.plot3D(x, y, z, 'purple')
+# ax.set_title('3D Line Plot')
+# plt.show()
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
 z = np.linspace(0, 1, 100)
 x = z * np.sin(25 * z)
 y = z * np.cos(25 * z)
+c = x + y  # Color array based on x and y
 
-ax.plot3D(x, y, z, 'purple')
-ax.set_title('3D Line Plot')
+ax.scatter(x, y, z, c=c)
+ax.set_title('3D Scatter Plot')
 plt.show()
