94c9dddb0a2ac21d3c5eea7a6c264af84ceadf5a
piyush <piyushcodes45@gmail.com>
2026-04-17 23:31:26 +0530

    seaborn

--- 3d graph .py
+++ 3d graph .py
@@ -1,36 +1,36 @@
-from mpl_toolkits import mplot3d
-import numpy as np
-import matplotlib.pyplot as plt
+# from mpl_toolkits import mplot3d
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# # fig = plt.figure()
+# # ax = plt.axes(projection='3d')
+
+# # z = np.linspace(0, 1, 100)
+# # x = z * np.sin(25 * z)
+# # y = z * np.cos(25 * z)
+
+# # ax.plot3D(x, y, z, 'purple')
+# # ax.set_title('3D Line Plot')
+# # plt.show()
+
+# # fig = plt.figure()
+# # ax = plt.axes(projection='3d')
+
+# # z = np.linspace(0, 1, 100)
+# # x = z * np.sin(25 * z)
+# # y = z * np.cos(25 * z)
+# # c = x + y  # Color array based on x and y
+
+# # ax.scatter(x, y, z, c=c)
+# # ax.set_title('3D Scatter Plot')
+# # plt.show()
+
+# x = np.outer(np.linspace(-2, 2, 10), np.ones(10))
+# y = x.copy().T
+# z = np.cos(x**2 + y**3)
 
 # fig = plt.figure()
 # ax = plt.axes(projection='3d')
-
-# z = np.linspace(0, 1, 100)
-# x = z * np.sin(25 * z)
-# y = z * np.cos(25 * z)
-
-# ax.plot3D(x, y, z, 'purple')
-# ax.set_title('3D Line Plot')
+# ax.plot_surface(x, y, z, cmap='viridis', edgecolor='green')
+# ax.set_title('Surface Plot')
 # plt.show()
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-
-# z = np.linspace(0, 1, 100)
-# x = z * np.sin(25 * z)
-# y = z * np.cos(25 * z)
-# c = x + y  # Color array based on x and y
-
-# ax.scatter(x, y, z, c=c)
-# ax.set_title('3D Scatter Plot')
-# plt.show()
-
-x = np.outer(np.linspace(-2, 2, 10), np.ones(10))
-y = x.copy().T
-z = np.cos(x**2 + y**3)
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot_surface(x, y, z, cmap='viridis', edgecolor='green')
-ax.set_title('Surface Plot')
-plt.show()
---
+++ graph2.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+tips = sns.load_dataset("tips")
+sns.violinplot(x="day", y="total_bill", data=tips, hue="sex", split=True)
+plt.title("Violin Plot of Total Bill by Day and Gender")
+plt.show()
