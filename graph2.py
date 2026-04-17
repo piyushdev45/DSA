aaad468ad57703d158b60f3e6718491db0767cff
piyush <piyushcodes45@gmail.com>
2026-04-17 23:32:57 +0530

    seaborn

--- graph2.py
+++ graph2.py
@@ -1,6 +1,12 @@
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# tips = sns.load_dataset("tips")
+# sns.violinplot(x="day", y="total_bill", data=tips, hue="sex", split=True)
+# plt.title("Violin Plot of Total Bill by Day and Gender")
+# plt.show()
 import seaborn as sns
 import matplotlib.pyplot as plt
-tips = sns.load_dataset("tips")
-sns.violinplot(x="day", y="total_bill", data=tips, hue="sex", split=True)
-plt.title("Violin Plot of Total Bill by Day and Gender")
+
+fmri = sns.load_dataset("fmri")
+sns.lineplot(x="timepoint", y="signal", hue="region", data=fmri)
 plt.show()
