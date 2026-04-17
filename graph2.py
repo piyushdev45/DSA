0072cde1d8219555a8bd1d51a799643d31eee1b4
piyush <piyushcodes45@gmail.com>
2026-04-17 23:34:10 +0530

    seaborn

--- graph2.py
+++ graph2.py
@@ -7,6 +7,11 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-fmri = sns.load_dataset("fmri")
-sns.lineplot(x="timepoint", y="signal", hue="region", data=fmri)
+# fmri = sns.load_dataset("fmri")
+# sns.lineplot(x="timepoint", y="signal", hue="region", data=fmri)
+# plt.show()
+
+tips = sns.load_dataset("tips")
+
+sns.scatterplot(x="total_bill", y="tip", hue="day", data=tips)
 plt.show()
