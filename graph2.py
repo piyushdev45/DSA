0d4f0caa7c4d7f245a2da2874c905e6800124468
piyush <piyushcodes45@gmail.com>
2026-04-17 23:37:18 +0530

    seaborn

--- graph2.py
+++ graph2.py
@@ -18,7 +18,15 @@ import matplotlib.pyplot as plt
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 
+# tips = sns.load_dataset("tips")
+
+# sns.barplot(x="day", y="total_bill", data=tips)
+# # plt.show()
+# next
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
 tips = sns.load_dataset("tips")
 
-sns.barplot(x="day", y="total_bill", data=tips)
+sns.boxplot(x="day", y="total_bill", data=tips)
 plt.show()
