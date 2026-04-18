25d51bfb1b02db6e023269251eb1e22218af5521
piyush <piyushcodes45@gmail.com>
2026-04-18 23:57:28 +0530

    seaborn

--- graph2.py
+++ graph2.py
@@ -41,6 +41,17 @@ import matplotlib.pyplot as plt
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 
-iris = sns.load_dataset("iris")
-sns.pairplot(iris, hue="species")
+# iris = sns.load_dataset("iris")
+# sns.pairplot(iris, hue="species")
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+x = ['sun', 'mon', 'fri', 'sat', 'tue', 'wed', 'thu']
+y = [5, 6.7, 4, 6, 2, 4.9, 1.8]
+
+ax = sns.stripplot(x=x, y=y)
+ax.set(xlabel='Days', ylabel='Amount Spent')
+plt.title('Daily Spending (Custom Data)')
 plt.show()
