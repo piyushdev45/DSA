b8b3f8fdcd0c4c54d6aae240c74112936f1461d9
piyush <piyushcodes45@gmail.com>
2026-04-21 23:32:46 +0530

    scipy

--- sci.py
+++ sci.py
@@ -1,9 +1,17 @@
+# import numpy as np
+# from scipy.linalg import det    
+# matrix = np.array([[1, 2, 3],
+#                    [0, 1, 4],
+#                    [5, 6, 0]])
+
+# determinant = det(matrix)
+
+# print(f"Determinant of the matrix is: {determinant}")
+
+from scipy import integrate
 import numpy as np
-from scipy.linalg import det    
-matrix = np.array([[1, 2, 3],
-                   [0, 1, 4],
-                   [5, 6, 0]])
 
-determinant = det(matrix)
 
-print(f"Determinant of the matrix is: {determinant}")
+f = lambda x: x**2
+result, error = integrate.quad(f, 0, 1)
+print(result)
