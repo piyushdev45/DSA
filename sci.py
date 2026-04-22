fc8286ad005284ad12eb845c8d5af407eb8858c7
piyush <piyushcodes45@gmail.com>
2026-04-22 21:42:25 +0530

    scipy

--- sci.py
+++ sci.py
@@ -8,10 +8,20 @@
 
 # print(f"Determinant of the matrix is: {determinant}")
 
-from scipy import integrate
-import numpy as np
+# from scipy import integrate
+# import numpy as np
 
 
-f = lambda x: x**2
-result, error = integrate.quad(f, 0, 1)
-print(result)
+# f = lambda x: x**2
+# result, error = integrate.quad(f, 0, 1)
+# print(result)
+
+import scipy.constants as const
+
+print("Pi:", const.pi)
+print("Golden ratio:", const.golden_ratio)
+print("Speed of light:", const.c)
+print("Gravitational constant:", const.G)
+print("Gas constant (R):", const.R)
+print("Boltzmann constant:", const.k)
+print("Proton mass:", const.proton_mass)
