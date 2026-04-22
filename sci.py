399c893f3a1d0629499bbf7743659f5836aace4e
piyush <piyushcodes45@gmail.com>
2026-04-22 21:48:52 +0530

    integration

--- sci.py
+++ sci.py
@@ -35,15 +35,26 @@
 # print(const.physical_constants['alpha particle mass'])
 
 
-import scipy.constants as const
+# import scipy.constants as const
 
-# Area of a circle using pi
-def area_of_circle(r):
-    return const.pi * r * r
+# # Area of a circle using pi
+# def area_of_circle(r):
+#     return const.pi * r * r
 
-# Gravitational force
-def force_gravity(M, m, dist):
-    return (const.G * M * m) / (dist ** 2)
+# # Gravitational force
+# def force_gravity(M, m, dist):
+#     return (const.G * M * m) / (dist ** 2)
 
-print("Area of Circle:", area_of_circle(5))
-print("Gravitational Force:", force_gravity(10, 5, 1))
+# print("Area of Circle:", area_of_circle(5))
+# print("Gravitational Force:", force_gravity(10, 5, 1))
+
+
+#integration using scipy
+from scipy.integrate import quad
+
+def f(x):
+    return 3 * x**2 + 1
+    
+I, err = quad(f, 0, 1)
+print(I)
+print(err)
