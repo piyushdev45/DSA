93ef074ab60ef75140ea8f4c61cdaaaab39c8d9e
piyush <piyushcodes45@gmail.com>
2026-04-23 17:10:29 +0530

    np and sci together

--- sci.py
+++ sci.py
@@ -71,5 +71,16 @@
 # print(cbrt(64))    
 # print(cbrt(78))
 
-from scipy.special import comb
-print(comb(4, 1))
+# from scipy.special import comb
+# print(comb(4, 1))
+
+# numpy and scipy are often used together for scientific computing. Here's an example of how to perform matrix addition using both libraries:
+import numpy as np
+from scipy.sparse import csr_matrix
+
+d = np.array([3, 4, 5, 7, 2, 6])     # data
+r = np.array([0, 0, 1, 1, 3, 3])     # rows
+c = np.array([2, 4, 2, 3, 1, 2])     # cols
+
+csr = csr_matrix((d, (r, c)), shape=(4, 5))
+print(csr.toarray())
